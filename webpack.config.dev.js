e511e020b2b70c170fcbacbbdb1330d034792979
Danfiff <danielmburu674@gmail.com>
2017-06-13 20:39:02 +0000

    replace styl with sass/sass for css

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,13 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
   entry: [
-    'webpack-hot-middleware/frontend',
-    './frontend/main'
+      'webpack-hot-middleware/frontend',
+      './frontend/main',
+      './frontend/styles/main.scss'
   ],
   output: {
     path: path.join(__dirname, '/static/dist/'),
@@ -14,7 +16,11 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      allChunks: true,
+    })
   ],
   module: {
     loaders: [
@@ -29,6 +35,16 @@ module.exports = {
       test: /\.styl$/,
       include: path.join(__dirname, 'frontend'),
       loader: 'style-loader!css-loader!stylus-loader'
+    },
+    { // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?importLoaders=1',
+        }),
+    },
+        { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
     }
     ]
   }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,11 +1,12 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
   entry: [
-
-    './frontend/main'
+      './frontend/main',
+      './frontend/styles/main.scss'
   ],
   output: {
     path: path.join(__dirname, '/static/dist/'),
@@ -25,6 +26,10 @@ module.exports = {
     }),
     new webpack.LoaderOptionsPlugin({
         minimize: true
+    }),
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      allChunks: true,
     })
   ],
   module: {
@@ -40,6 +45,16 @@ module.exports = {
       test: /\.styl$/,
       include: path.join(__dirname, 'frontend'),
       loader: 'style-loader!css-loader!stylus-loader'
+    },
+    { // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?importLoaders=1',
+        }),
+    },
+        { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
     }
     ]
   }
